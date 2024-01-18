7caefbc69546ac00e14467b5958bc3f16cee283d
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2024-01-18 20:38:02 -0300

    add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: "#ECEDEE",
+          100: "#DCDDDE",
+          200: "#B9BBBE",
+          300: "#8E9297",
+          400: "#72767D",
+          500: "#5C6067",
+          600: "#464950",
+          700: "#36393F",
+          800: "#2F3136",
+          900: "#202225",
+        },
+      },
+    },
   },
   plugins: [],
 }
